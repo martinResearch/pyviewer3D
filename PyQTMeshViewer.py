53f27953e67a859d9b385b33ffa7f30886899c97
martin_delagorce <martin_delagorce@localhost>
2013-03-27 22:41:52 +0100

    ability to display points

--- PyQTMeshViewer.py
+++ PyQTMeshViewer.py
@@ -3,7 +3,8 @@
 #   http://www.siafoo.net/snippet/316
 #   http://pyqglviewer.gforge.inria.fr/wiki/doku.php?id=examples # could not install it....
 # QT file loader
-# collada example print_collada_info.py
+# collada example print_collada_info.py 
+# could have a look at http://rossant.github.com/galry/ that seem to be able to plot multi miliions of points in python
 
 import sys
 from PyQt4 import QtGui
@@ -12,11 +13,12 @@ from OpenGL.GLU import *
 from PyQt4 import QtGui
 from PyQt4.QtOpenGL import *
 from PyGLWidget import PyGLWidget
-import collada #from  https://github.com/pycollada/pycollada
+#import collada #from  https://github.com/pycollada/pycollada
 import meshconvert # from https://code.google.com/p/meshconvert/
 import math
 import os
-
+import numpy
+import OpenGL.arrays.vbo as glvbo
 
 
 class MeshWidget(PyGLWidget):
@@ -24,26 +26,41 @@ class MeshWidget(PyGLWidget):
     def __init__(self, parent):
         PyGLWidget.__init__(self, parent)
         self.col=[]
+        self.points= []
 
-    def loadPoint(points)
-        self.points=points
-
+    def plotPoints(self,points):
+        self.points=numpy.array(points,dtype=numpy.float32)
+        self.scale=1
+        #self.faces=numpy.array([[1,2,3],[3,4,5]],dtype=numpy.int32)
+        # create a Vertex Buffer Object with the specified data
+        self.vbo = glvbo.VBO(self.points)    # from # http://cyrille.rossant.net/blog/page/3/    
+        
     def paintGL(self):
         PyGLWidget.paintGL(self)
 
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
         glShadeModel(GL_SMOOTH)
-        glEnable(GL_LIGHT0)
-        glEnable(GL_LIGHTING)
-        scale=0.01
+        #glEnable(GL_LIGHT0)
+        #glEnable(GL_LIGHTING)
+        
 
        
         #glEnableClientState(GL_NORMAL_ARRAY)
         #glNormalPointerf( prim.normal)#http://www.opengl.org/discussion_boards/ubbthreads.php?ubb=showflat&Number=264532
-        glEnableClientState(GL_VERTEX_ARRAY)
-        glVertexPointerf( prim.vertex*scale)
-
-        glDrawElementsui(GL_TRIANGLES,prim.vertex_index)
+      
+        if len(self.points)>0:
+                         
+            glColor( 0.95, 0.207, 0.031 )
+            glPointSize( 6.0 )
+            glEnable( GL_POINT_SMOOTH )
+            
+            glEnableClientState(GL_VERTEX_ARRAY)#tell OpenGL that the VBO contains an array of vertices
+            glVertexPointerf( self.points*self.scale)
+            #self.vbo.bind() # bind the VBO 
+            #glVertexPointer(3, GL_FLOAT, 0, self.vbo) # these vertices contain 3 single precision coordinates
+            glDrawArrays(GL_POINTS, 0, self.points.shape[0])   # draw "count" points from the VBO
+            #glDrawElementsui(GL_POINTS,self.points*self.scale)
+            #glDrawElementsui(GL_TRIANGLES,self.faces)
 
 ##                        glBegin(GL_TRIANGLES)                 # Start drawing a polygon
 ##                        for tri  in prim.triangles():
@@ -74,7 +91,7 @@ class Example(QtGui.QMainWindow):
 
     def initUI(self):
 
-        self.glWidget = ColladaWidget(self)
+        self.glWidget = MeshWidget(self)
         self.setCentralWidget(self.glWidget)
         self.statusBar()
 
@@ -102,6 +119,7 @@ class Example(QtGui.QMainWindow):
         vertices=[]
         for t in r.readNode():
             vertices.append([t.x,t.y,t.z])
+        self.glWidget.plotPoints(vertices)    
         # creat numpy arrays
         # vertices
         # normals
