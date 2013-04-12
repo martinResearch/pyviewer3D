221c89643d4595ca9d9335041863b963bb2ed710
martin_delagorce <martin_delagorce@localhost>
2013-04-12 20:14:49 +0200

    fixing default color

--- meshconvert/generic.py
+++ meshconvert/generic.py
@@ -33,7 +33,7 @@ class reader(object):
 		self.lineno = 0
 
 class node(object):
-	def __init__(self, x, y, z, label='1', color=['0.5','0.5','0.5'],normal=[]):
+	def __init__(self, x, y, z, label='1', color=['255','255','255'],normal=[]):
 		"Mesh node"
 		self.x = x
 		self.y = y
